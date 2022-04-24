31ccf1b7e731234a448267937390b0b1d76ce00f
Oleksandr Yashin <aveodenis@gmail.com>
2022-04-24 20:48:04 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://n1ke23.github.io/todolistts/
 export default defineConfig({
   plugins: [react()]
 })
